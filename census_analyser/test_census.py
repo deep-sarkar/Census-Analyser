53fb4a573355bc4bbd39f1e092c7cef405d1cb9e
deep-sarkar <deepsarkar96@gmail.com>
2020-07-09 13:33:51 +0530

    [Deep]Add.added test for number of records

---
+++ census_analyser/test_census.py
@@ -0,0 +1,9 @@
+import pytest
+from . stateCensusAnalyser import CSVStateCensus
+
+class TestCensus:
+
+    def test_State_census_records_to_match_number_of_records_UC1(self):
+        obj = CSVStateCensus("IndiaStateCensusData.csv")
+        total_records = obj.number_of_records(obj.df)
+        assert total_records == 28
