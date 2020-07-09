9988369e5422f9b8102b42fb3b88a85320b5927f
deep-sarkar <deepsarkar96@gmail.com>
2020-07-09 16:47:16 +0530

    [Deep]Add.added test for empty file TC3

--- census_analyser/test_census.py
+++ census_analyser/test_census.py
@@ -1,14 +1,20 @@
 import pytest
 from stateCensusAnalyser import CSVStateCensus
-from custom_exceptions import FileIsNotCSVTypeException
+from custom_exceptions import FileIsNotCSVTypeException,EmptyFileException
 
 class TestCensus:
 
     def test_State_census_records_to_match_number_of_records_UC1_TC1(self):
         obj = CSVStateCensus("IndiaStateCensusData.csv")
-        total_records = obj.number_of_records(obj.df)
+        total_records = obj.number_of_records(obj.load_CSV)
         assert total_records == 28
 
     def test_file_not_in_csv_format_will_raise_FileIsNotCSVTypeException_UC1_TC2(self):
         with pytest.raises(FileIsNotCSVTypeException):
-            CSVStateCensus("abc.txt")
+            obj = CSVStateCensus("demo.txt")
+            obj.load_CSV
+    
+    def test_file_is_csv_but_empty_will_raise_EmptyFileException_UC1_TC3(self):
+        with pytest.raises(EmptyFileException):
+            obj = CSVStateCensus("demo.csv")
+            obj.load_CSV
