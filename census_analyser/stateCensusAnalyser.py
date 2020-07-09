d38fbd607f5be36cf246b393081e309d9a376b77
deep-sarkar <deepsarkar96@gmail.com>
2020-07-09 13:19:48 +0530

    [Deep]Add.added iterator method for data frame

--- census_analyser/stateCensusAnalyser.py
+++ census_analyser/stateCensusAnalyser.py
@@ -18,14 +18,26 @@ class CSVStateCensus(StateCensusAnalyser):
         self.col_list = repr(StateCensusAnalyser()).split(",")
         self.df = pd.read_csv(self.file_name, usecols=self.col_list)
 
+    def iterate_df(self, df):
+        for row in df.itertuples():
+            print(row)
+        
+
 
 file_name = "IndiaStateCensusData.csv"
 obj = CSVStateCensus(file_name)
 df = obj.df
-df_goa = df.loc[df["State"]=="Goa"]
-print(df_goa)
-print(df_goa['Population'])
+# obj.iterate_df(df)
+# df_goa = df.loc[df["State"]=="Goa"]
+# print(df_goa)
+# print(df_goa['Population'])
 
+    
+  
+# for ind in df.index:
+#     print(df['State'][ind])
+
+    
 
 
 
