471a768e0b708fd20cb8f5c11e20fa84d11dca7f
deep-sarkar <deepsarkar96@gmail.com>
2020-07-09 13:36:36 +0530

    [Deep]Refactor.modified test name for TC1

--- census_analyser/test_census.py
+++ census_analyser/test_census.py
@@ -3,7 +3,7 @@ from . stateCensusAnalyser import CSVStateCensus
 
 class TestCensus:
 
-    def test_State_census_records_to_match_number_of_records_UC1(self):
+    def test_State_census_records_to_match_number_of_records_UC1_TC1(self):
         obj = CSVStateCensus("IndiaStateCensusData.csv")
         total_records = obj.number_of_records(obj.df)
         assert total_records == 28
