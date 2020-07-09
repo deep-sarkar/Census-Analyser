441497f7634818cd86b4f0ebee8ea0cbd1f97d1a
deep-sarkar <deepsarkar96@gmail.com>
2020-07-09 12:40:17 +0530

    [Deep]Add.added CSVStateCensus clas to load csv

--- census_analyser/stateCensusAnalyser.py
+++ census_analyser/stateCensusAnalyser.py
@@ -1,3 +1,4 @@
+import pandas as pd
 class StateCensusAnalyser:
 
     def __init__(self):
@@ -8,5 +9,21 @@ class StateCensusAnalyser:
 
     def __repr__(self):
         return self.state +','+ self.population +','+ self.areaInSqKm +','+ self.densityPerSqKm
+
+
+class CSVStateCensus(StateCensusAnalyser):
+
+    def __init__(self):
+        self.file_name = "IndiaStateCensusData.csv"
+        self.col_list = repr(StateCensusAnalyser()).split(",")
+        self.df = pd.read_csv(self.file_name, usecols=self.col_list)
+
+# obj = CSVStateCensus()
+# df = obj.df
+# df_goa = df.loc[df["State"]=="Goa"]
+# print(df_goa['Population'])
+# print(obj.df.iloc["Goa"])
+# if obj.df["State"] == "Goa":
+#     print(obj.df['Population'])
 
 
